1d77137edb4f2ab292b7da944b52ba20092670b1
poletaeeva <kris300977@gmail.com>
2026-04-10 01:06:10 +0300

    add: eda template

---
+++ eda.py
@@ -0,0 +1,17 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import re
+import logging
+from collections import Counter
+
+logger = logging.getLogger(__name__)
+
+
+def run_eda():
+    pass
+
+
+if __name__ == "__main__":
+    run_eda()
